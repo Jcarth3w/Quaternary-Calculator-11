a9f2063ab1ff6d0f84953d51cbbd0daa30399558
Andrew <andreweveragescheible@gmail.com>
2023-09-05 08:03:26 -0400

    Add basic calculator layout and button actions

--- src/main/java/UI/Application.java
+++ src/main/java/UI/Application.java
@@ -9,9 +9,9 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("calculator-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 200, 250);
+        stage.setTitle("QuaternaryCalculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/UI/Controller.java
+++ src/main/java/UI/Controller.java
@@ -1,14 +1,49 @@
 package UI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField calculationTextField;
+
+    @FXML
+    public void onZeroButtonPress(ActionEvent actionEvent) {
+        System.out.println("0");
+    }
+
+    public void onOneButtonPress(ActionEvent actionEvent) {
+        System.out.println("1");
+    }
+
+    public void onTwoButtonPress(ActionEvent actionEvent) {
+        System.out.println("2");
+    }
+
+    public void onThreeButtonPress(ActionEvent actionEvent) {
+        System.out.println("3");
+    }
+
+    public void onDivideButtonPress(ActionEvent actionEvent) {
+        System.out.println("/");
+    }
+
+    public void onMultiplyButtonPress(ActionEvent actionEvent) {
+        System.out.println("*");
+    }
+
+    public void onSubtractButtonPress(ActionEvent actionEvent) {
+        System.out.println("-");
+    }
+
+    public void onAddButtonPress(ActionEvent actionEvent) {
+        System.out.println("+");
+    }
+
+    public void onEqualButtonPress(ActionEvent actionEvent) {
+        System.out.println("=");
     }
 }
